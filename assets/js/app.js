d2c8ebc9d88272e000e8f77ecc462f383cdb802f
nakasone <40297126+nakasoneryouta@users.noreply.github.com>
2021-05-28 05:51:16 +0900

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,18 @@ $(window).scroll(function (){
    }
  });
 });
+
+
+//scrollの位置を修正
+$(function () {
+ var headerHight = 200; //ヘッダの高さ
+ $('a[href^="#"]').click(function(){
+     var href= $(this).attr("href");
+       var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
+     $("html, body").animate({scrollTop:position}, 550, "swing");
+        return false;
+   });
+});
+
+AOS.init();
